2d5e6502911c564ba2eedfe72ebdb632bf485163
vehikl <go@vehikl.com>
2021-11-09 14:08:06 -0500

    Prevent guests from getting the list of users

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('auth')->get('/users', fn() => \App\Models\User::all());
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,32 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+        User::factory()->times(20)->create();
+    }
+
+    public function test_a_guest_cannot_get_the_list_of_users()
+    {
+        $this->getJson('/users')->assertUnauthorized();
+    }
+
+    public function test_a_user_can_get_the_list_of_users()
+    {
+        $theAuthenticatedUser = User::query()->first();
+
+        $this->actingAs($theAuthenticatedUser)
+            ->getJson('/users')
+            ->assertSuccessful()
+            ->assertJson(User::all()->toArray());
     }
 }
